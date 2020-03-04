b2c467de89da23ec73d290038f985e570d631e1b
DanielMR <jadaniel54@gmal.com>
2020-03-04 12:02:32 -0700

    allModelFiles

---
+++ PlaylistCoredata/PlaylistCoredata/Model/Song+Convenience.swift
@@ -0,0 +1,21 @@
+//
+//  Song+Convenience.swift
+//  PlaylistCoredata
+//
+//  Created by Daniel Merchan Rico on 3/4/20.
+//  Copyright © 2020 Trevor Walker. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Song {
+    
+    convenience init(title: String, artist: String, playlist: Playlist, moc: NSManagedObjectContext = CoreDataStack.context) {
+        
+        self.init(context: moc)
+        self.title = title
+        self.artist = artist
+        self.playlist = playlist
+    }
+}
